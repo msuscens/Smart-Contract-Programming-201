dbfad36ae0682bda9d48f165eb263e8c65a80edd
Mark Suscens <mark.suscens@gmail.com>
2021-05-04 20:32:04 +0100

    Create basic DEX contract structure (untested)

---
+++ DEX-Project/contracts/Dex.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+// TODO - import Wallet (via an interface??)
+import "./Wallet.sol";
+
+contract Dex is Wallet {
+
+    enum Side {
+        BUY,
+        SELL
+    }
+
+    struct Order {
+        uint id;
+        address trader;
+        Side side;
+        bytes32 ticker;
+        uint amount;
+        uint price; 
+    }
+    // order.side = Side.BUY;
+
+    mapping(bytes32 => mapping(uint => Order[])) orderBook;    //Ticker => (Side => orders)
+
+    function getOrderBook(bytes32 ticker, Side side) view public returns(Order[] memory) {
+        return(orderBook[ticker][uint(side)]);
+    }
+    // Note: Call would be :
+    //  getOrderBook(bytes32("LINK"), Side.BUY);
+
+
+    function createLimitOrder() public {
+        // TODO
+    }
+
+}
